1e8da504bf5516c1faa5eec5f54bebbb808d17f2
Paul Unsted <ed__84@hotmail.com>
2019-10-08 18:27:35 -0400

    Began implementation of handlebars.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var logger = require("morgan");
 var mongoose = require("mongoose");
+var exphbs = require("express-handlebars");
 
 // Our scraping tools
 // Axios is a promised-based http library, similar to jQuery's Ajax method
@@ -15,6 +16,9 @@ var PORT = 3000;
 
 // Initialize Express
 var app = express();
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 // Configure middleware
 
@@ -65,9 +69,7 @@ app.get("/scrape", function(req, res) {
         .children("picture")
         .children("img")
         .attr("src");
-
-      // Ok, images are being scraped properly unless they're in compact articles. Adding authors and publications dates to scraped articles is the next step.
-
+      // Ok, images are being scraped properly unless they're in compact articles. Adding authors and publications dates to scraped articles is the next step. OR IS IT
       if (result.image === undefined) {
         result.image = "No image found.";
         // result.image = $(this)
@@ -76,6 +78,11 @@ app.get("/scrape", function(req, res) {
         //   .children("img")
         //   .attr("src");
       }
+      result.author = $(this);
+
+      // A lot of children here to be wrangled. Invest time into handlebars and deploying this before spending more time on increased functionality, you jerk. It's more important to have a working site than it is to have a working site with wide functionality than it is to have a good-looking working site with wide functionality.
+
+      // Get this fucking project up to MVP and deploy it before you spend hours unravelling spaghetti code FFS. Go looking for authors and publication dates after you get things running at their most basic, and in this case populating the site with handlebars objects.
 
       // Create a new Article using the `result` object built from scraping
       db.Article.create(result)
