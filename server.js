0481e945b49ebd728e811357b9949b58f16654cb
Paul Unsted <ed__84@hotmail.com>
2019-10-20 18:35:55 -0400

    Refined index.handlebars to build articles properly as list items. Moved css into its own file and styled things so that they look less like a pile of assholes. Notes are ostensibly working, but aren't appearing where I want them to. Work continues.

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Make public a static folder
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "/public")));
 
 // Connect to the Mongo DB
 mongoose.connect("mongodb://localhost/VoxScrape", {
